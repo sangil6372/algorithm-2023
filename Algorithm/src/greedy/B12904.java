7f97b9d25b595575c71ec680c9996937be989977
박상일 <sangil6372@naver.com>
2023-06-04 21:34:11 +0900

    .

--- Algorithm/src/greedy/B12904.java
+++ Algorithm/src/greedy/B12904.java
@@ -13,10 +13,8 @@ public class B12904 {
 //		문자열의 뒤에 A를 추가한다.
 //		문자열을 뒤집고 뒤에 B를 추가한다.
 		
-		
+//		생각의 전환...! S를 생각하지 말고 T를 하나씩 줄여가보자 마지막 단계는 예측할 수 있으니!
 		while (S.length()<T.length()) {
-//		A를 붙이거나  =  ABA     뒤집고 B를 붙임  BAB    
-//			일단 T에서 sb 찾고 있으면 ok 없으면 flag = 0
 			if (T.charAt(T.length()-1)=='A') {
 				T.deleteCharAt(T.length()-1);
 			}
@@ -25,8 +23,7 @@ public class B12904 {
 				T.reverse();
 			}
 		}
-		System.out.println(S);
-		System.out.println(T);
+		
 		if (S.toString().equals(T.toString())) {
 			System.out.println('1');
 		}else {
